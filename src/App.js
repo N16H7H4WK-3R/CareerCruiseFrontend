1db436e0ca91dd40f7ecc4a15295c34d2845f61e
N16H7H4WK-3R <aryan014kumar@gmail.com>
2023-10-19 18:28:23 +0530

    updated buttoms and added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-// import LandingPage from './components/landing_page';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/landing_page';
 import LoginPage from './components/login_page';
 
 function App() {
   return (
-    <div>
-      {/* <LandingPage /> */}
-      <LoginPage />
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -11,6 +11,10 @@ function LandingPage() {
                             A job board website where employers can post job openings and job seekers can search and apply for jobs.
                         </p>
                     </header>
+                    <div className='main' >
+                        <button>Employer</button>
+                        <button>Company</button>
+                    </div>
                 </section>
                 <section className="banner2">
                     <img src="https://images.unsplash.com/photo-1543158266-0066955047b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80" height="300" width="450" alt='one' />
