2f328247a5fd375738ecaa75debd83a4ab65eecc
N16H7H4WK-3R <aryan014kumar@gmail.com>
2023-10-20 08:24:11 +0530

    added validations

--- src/components/login_page.js
+++ src/components/login_page.js
@@ -1,76 +1,137 @@
 import React, { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage(props) {
     const [activeForm, setActiveForm] = useState('login');
+    const [loginEmail, setLoginEmail] = useState('');
+    const [loginPassword, setLoginPassword] = useState('');
+    const [signupEmail, setSignupEmail] = useState('');
+    const [signupPassword, setSignupPassword] = useState('');
+    const [signupPasswordConfirm, setSignupPasswordConfirm] = useState('');
+    const navigate = useNavigate();
+
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const passwordMinLength = 6;
 
     const switchForm = (form) => {
         setActiveForm(form);
     };
 
+    const validateEmail = (email) => {
+        return emailRegex.test(email);
+    };
+
+    const validatePassword = (password) => {
+        return password.length >= passwordMinLength;
+    };
+
+    const handleLogin = (e) => {
+        if (validateEmail(loginEmail) && validatePassword(loginPassword)) {
+            navigate('/user');
+        }
+    };
+
+    const handleSignup = (e) => {
+        if (validateEmail(signupEmail) && validatePassword(signupPassword) && signupPassword === signupPasswordConfirm) {
+            navigate('/');
+        }
+    };
+
     return (
         <div className="login_screen">
-        <section className="forms-section">
-            <h1 class="section-title">{props.name} Credentials Page</h1>
-            <div className="forms">
-                <div className={`form-wrapper ${activeForm === 'login' ? 'is-active' : ''}`}>
-                    <button
-                        type="button"
-                        className="switcher switcher-login"
-                        onClick={() => switchForm('login')}
-                    >
-                        Login
-                        <span className="underline"></span>
-                    </button>
-                    <form className="form form-login">
-                        <fieldset>
-                            <legend>Please, enter your email and password for login.</legend>
-                            <div className="input-block">
-                                <label htmlFor="login-email">E-mail</label>
-                                <input id="login-email" type="email" spellcheck="false" required autocomplete="off" />
-                            </div>
-                            <div className="input-block">
-                                <label htmlFor="login-password">Password</label>
-                                <input id="login-password" type="password" required />
-                            </div>
-                        </fieldset>
-                        <button type="submit" className="btn-login">
+            <section className="forms-section">
+                <h1 className="section-title">{props.name} Credentials Page</h1>
+                <div className="forms">
+                    <div className={`form-wrapper ${activeForm === 'login' ? 'is-active' : ''}`}>
+                        <button
+                            type="button"
+                            className="switcher switcher-login"
+                            onClick={() => switchForm('login')}
+                        >
                             Login
+                            <span className="underline"></span>
                         </button>
-                    </form>
-                </div>
-                <div className={`form-wrapper ${activeForm === 'signup' ? 'is-active' : ''}`}>
-                    <button
-                        type="button"
-                        className="switcher switcher-signup"
-                        onClick={() => switchForm('signup')}
-                    >
-                        Sign Up
-                        <span className="underline"></span>
-                    </button>
-                    <form className="form form-signup">
-                        <fieldset>
-                            <legend>Please, enter your email, password, and password confirmation for sign up.</legend>
-                            <div className="input-block">
-                                <label htmlFor="signup-email">E-mail</label>
-                                <input id="signup-email" type="email" required />
-                            </div>
-                            <div className="input-block">
-                                <label htmlFor="signup-password">Password</label>
-                                <input id="signup-password" type="password" required />
-                            </div>
-                            <div className="input-block">
-                                <label htmlFor="signup-password-confirm">Confirm password</label>
-                                <input id="signup-password-confirm" type="password" required />
-                            </div>
-                        </fieldset>
-                        <button type="submit" className="btn-signup">
-                            Continue
+                        <form className="form form-login">
+                            <fieldset>
+                                <legend>Please, enter your email and password for login.</legend>
+                                <div className="input-block">
+                                    <label htmlFor="login-email">E-mail</label>
+                                    <input
+                                        id="login-email"
+                                        type="email"
+                                        spellCheck="false"
+                                        required
+                                        autoComplete="off"
+                                        value={loginEmail}
+                                        onChange={(e) => setLoginEmail(e.target.value)}
+                                    />
+                                </div>
+                                <div className="input-block">
+                                    <label htmlFor="login-password">Password</label>
+                                    <input
+                                        id="login-password"
+                                        type="password"
+                                        required
+                                        value={loginPassword}
+                                        onChange={(e) => setLoginPassword(e.target.value)}
+                                    />
+                                </div>
+                            </fieldset>
+                            <button type="submit" onClick={handleLogin} className="btn-login" disabled={!validateEmail(loginEmail) || !validatePassword(loginPassword)}>
+                                Login
+                            </button>
+                        </form>
+                    </div>
+                    <div className={`form-wrapper ${activeForm === 'signup' ? 'is-active' : ''}`}>
+                        <button
+                            type="button"
+                            className="switcher switcher-signup"
+                            onClick={() => switchForm('signup')}
+                        >
+                            Sign Up
+                            <span className="underline"></span>
                         </button>
-                    </form>
+                        <form className="form form-signup">
+                            <fieldset>
+                                <legend>Please, enter your email, password, and password confirmation for sign up.</legend>
+                                <div className="input-block">
+                                    <label htmlFor="signup-email">E-mail</label>
+                                    <input
+                                        id="signup-email"
+                                        type="email"
+                                        required
+                                        value={signupEmail}
+                                        onChange={(e) => setSignupEmail(e.target.value)}
+                                    />
+                                </div>
+                                <div className="input-block">
+                                    <label htmlFor="signup-password">Password</label>
+                                    <input
+                                        id="signup-password"
+                                        type="password"
+                                        required
+                                        value={signupPassword}
+                                        onChange={(e) => setSignupPassword(e.target.value)}
+                                    />
+                                </div>
+                                <div className="input-block">
+                                    <label htmlFor="signup-password-confirm">Confirm password</label>
+                                    <input
+                                        id="signup-password-confirm"
+                                        type="password"
+                                        required
+                                        value={signupPasswordConfirm}
+                                        onChange={(e) => setSignupPasswordConfirm(e.target.value)}
+                                    />
+                                </div>
+                            </fieldset>
+                            <button type="submit" onClick={handleSignup} className="btn-signup" disabled={!validateEmail(signupEmail) || !validatePassword(signupPassword) || signupPassword !== signupPasswordConfirm}>
+                                Continue
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         </div>
     );
 }
